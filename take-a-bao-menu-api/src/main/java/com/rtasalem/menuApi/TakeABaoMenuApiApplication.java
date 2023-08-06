1bc8c359a56f405c3a0162a8bc949aec410007c9
Rana Salem <ranatasalem@gmail.com>
2023-08-06 20:23:13 +0100

    Initial commit

---
+++ take-a-bao-menu-api/src/main/java/com/rtasalem/menuApi/TakeABaoMenuApiApplication.java
@@ -0,0 +1,13 @@
+package com.rtasalem.menuApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeABaoMenuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeABaoMenuApiApplication.class, args);
+	}
+
+}
---
+++ take-a-bao-menu-api/src/test/java/com/rtasalem/menuApi/TakeABaoMenuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtasalem.menuApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeABaoMenuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
