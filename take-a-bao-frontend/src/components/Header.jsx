0a5018aec5f88da6de4b70bf2a5b13c90b0809d0
Rana Salem <ranatasalem@gmail.com>
2023-08-16 11:02:17 +0100

    imported bootstrap styling to fully implement bootstrap within react

--- take-a-bao-frontend/src/components/Header.jsx
+++ take-a-bao-frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import "../styles/Header.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Header = () => {
   return (
