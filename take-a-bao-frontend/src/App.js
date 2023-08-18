032b2d5b9451fceb183d316091e10069c7002395
Rana Salem <ranatasalem@gmail.com>
2023-08-18 13:24:15 +0100

    First draft of landing page complete

--- take-a-bao-frontend/src/App.js
+++ take-a-bao-frontend/src/App.js
@@ -2,13 +2,12 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
-import AboutUsPage from './pages/AboutUsPage';
+import EventsPage from './pages/EventsPage';
 import BaoziPage from './pages/BaoziPage';
 import SidesPage from './pages/SidesPage';
 import DessertsPage from './pages/DessertsPage';
 import DrinksPage from './pages/DrinksPage';
 import Footer from './components/Footer';
-import FAQsPage from './pages/FAQsPage';
 
 function App() {
   return (
@@ -16,12 +15,11 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/about-us' element={<AboutUsPage />} />
+        <Route path='/events' element={<EventsPage />} />
         <Route path='/baozi' element={<BaoziPage />} />
         <Route path='/sides' element={<SidesPage />} />
         <Route path='/desserts' element={<DessertsPage />} />
         <Route path='/drinks' element={<DrinksPage />} />
-        <Route path='/FAQs' element={<FAQsPage />} />
       </Routes>
       <Footer />
     </div>
---
+++ take-a-bao-frontend/src/pages/EventsPage.jsx
@@ -0,0 +1,8 @@
+const EventsPage = () => {
+  return (
+    <div>
+      <h1>events page</h1>
+    </div>
+  );
+};
+export default EventsPage;
