d6bbb40916a21fece37ebfd798802b626dbe393a
Rana Salem <ranatasalem@gmail.com>
2023-09-08 14:57:37 +0100

    Adding HTTP requests in the EventsController and associated methods in the EventsService.

--- take-a-bao-api/src/main/java/com/rtasalem/menuApi/controller/EventsController.java
+++ take-a-bao-api/src/main/java/com/rtasalem/menuApi/controller/EventsController.java
@@ -1,10 +1,25 @@
 package com.rtasalem.menuApi.controller;
 
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.rtasalem.menuApi.model.Events;
 import com.rtasalem.menuApi.service.EventsService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/events")
@@ -13,14 +28,34 @@ public class EventsController {
 	
 	EventsService eventsService;
 
-	// Using constructor injection
+	// Constructor injection
 	public EventsController(EventsService eventsService) {
 		super();
 		this.eventsService = eventsService;
 	}
 
+	@GetMapping
+	public ResponseEntity<List<Events>> getAllEvents() {
+		return ResponseEntity.status(HttpStatus.OK).body(eventsService.findAllEvents());
+	}
 	
+	@PostMapping
+	public ResponseEntity<Events> addNewEvent(@Valid @RequestBody Events event) {
+		eventsService.createEvent(event);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{eventId}").buildAndExpand(event.getEventId()).toUri();
+		return ResponseEntity.created(location).body(event);
+	}
 	
+	@PutMapping("/{eventId}")
+	public ResponseEntity<Events> updateEvent(@PathVariable int eventId, @Valid @RequestBody Events event) {
+		return ResponseEntity.ok(eventsService.editEvent(event, eventId));
+	}
+	
+	@DeleteMapping("/{eventId}")
+	public ResponseEntity<Void> deleteEvent(@PathVariable int eventId) {
+		eventsService.removeEventByEventId(eventId);
+		return ResponseEntity.status(HttpStatus.OK).build();
+	}
 	
 
 }
--- take-a-bao-api/src/main/java/com/rtasalem/menuApi/service/EventsService.java
+++ take-a-bao-api/src/main/java/com/rtasalem/menuApi/service/EventsService.java
@@ -1,7 +1,11 @@
 package com.rtasalem.menuApi.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.rtasalem.menuApi.exception.ResourceNotFoundException;
+import com.rtasalem.menuApi.model.Events;
 import com.rtasalem.menuApi.repository.EventsDAO;
 
 @Service
@@ -15,7 +19,30 @@ public class EventsService {
 		super();
 		this.eventsRepo = eventsRepo;
 	}
+
+	public List<Events> findAllEvents() {
+		return eventsRepo.findAll();
+	}
 	
+	public Events createEvent(Events event) {
+		return eventsRepo.save(event);
+	}
 	
+	public Events editEvent(Events event, int eventId) {
+		
+		if (!eventsRepo.existsById(eventId)) {
+			throw new ResourceNotFoundException("Event with ID of " + eventId + " does not exist.");
+		}
+		event.setEventId(eventId);
+		return eventsRepo.save(event);
+	}
+	
+	public void removeEventByEventId(int eventId) {
+		
+		if (!eventsRepo.existsById(eventId)) {
+			throw new ResourceNotFoundException("Event with ID of " + eventId + " does not exist.");
+		}
+		eventsRepo.deleteById(eventId);
+	}
 
 }
