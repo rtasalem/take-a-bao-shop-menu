8da047bd17cad4fab4fabec29193f582d293e0fa
Rana Salem <ranatasalem@gmail.com>
2024-03-10 22:29:16 +0000

    add passing tests to controller layer

--- take-a-bao-menu-api/src/test/java/com/rtasalem/menuApi/controllerTests/MenuItemControllerTests.java
+++ take-a-bao-menu-api/src/test/java/com/rtasalem/menuApi/controllerTests/MenuItemControllerTests.java
@@ -19,19 +19,17 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
-import com.rtasalem.menuApi.model.MenuItem;
 import com.rtasalem.menuApi.controller.MenuItemController;
+import com.rtasalem.menuApi.model.MenuItem;
 import com.rtasalem.menuApi.service.MenuItemService;
 
 @ExtendWith(MockitoExtension.class)
@@ -53,7 +51,7 @@ class MenuItemControllerTests {
 
 	@BeforeEach
 	public void setUp() {
-		mockMvc = MockMvcBuilders.standaloneSetup(menuItemController).build();
+		mockMvc = MockMvcBuilders.standaloneSetup(mockMenuItemController).build();
 	}
 
 	@Test
@@ -61,17 +59,83 @@ class MenuItemControllerTests {
 		// Arrange
 		MenuItem item1 = createMenuItem(1);
 		MenuItem item2 = createMenuItem(2);
-		List<MenuItem> menuItems = new Arrays.asList(item1, item2);
+		List<MenuItem> menuItems = Arrays.asList(item1, item2);
 		when(mockMenuItemService.findAllMenuItems()).thenReturn(menuItems);
-
+		
 		// Act
 		ResponseEntity<List<MenuItem>> response = mockMenuItemController.getAllMenuItems();
-
-		// Assert
-		assertEquals(HttpStaus.OK, response.getStatusCode());
+		
+		// Arrange
+		assertEquals(HttpStatus.OK, response.getStatusCode());
 		assertEquals(menuItems, response.getBody());
 		verify(mockMenuItemService, times(1)).findAllMenuItems();
 	}
 
-
+	@Test
+	public void testGetMenuItemByItemId_ReturnsMenuItemAndOkResponse() {
+		// Arrange
+		int itemId = 1;
+		MenuItem item = createMenuItem(itemId);
+		when(mockMenuItemService.findMenuItemById(itemId)).thenReturn(item);
+		
+		// Act
+		ResponseEntity<MenuItem> response = mockMenuItemController.getMenuItemById(itemId);
+		
+		// Assert
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals(item, response.getBody());
+		verify(mockMenuItemService, times(1)).findMenuItemById(itemId);
+	}
+	
+	@Test
+	public void testAddNewMenuItem_CreatesNewMenuItemSuccessfully() throws Exception {
+		// Arrange
+		MenuItem item = new MenuItem("Char Siu Bao", "Very tasty choice", 1.99, "main","url-for-menu-item-image", false);
+		ArgumentCaptor<MenuItem> captor = ArgumentCaptor.forClass(MenuItem.class);
+		when(mockMenuItemService.createMenuItem(captor.capture())).thenReturn(item);
+		
+		// Act
+		mockMvc.perform(post("/api/v1/menu-items").contentType(MediaType.APPLICATION_JSON)
+				.content(new ObjectMapper().writeValueAsString(item)))
+		// Assert
+		.andExpect(status().isCreated())
+		.andExpect(jsonPath("$.itemName").value("Char Siu Bao"))
+		.andExpect(jsonPath("$.itemDescription").value("Very tasty choice"))
+		.andExpect(jsonPath("$.itemPrice").value("1.99"))
+		.andExpect(jsonPath("$.itemCategory").value("main"))
+		.andExpect(jsonPath("$.itemImage").value("url-for-menu-item-image"))
+		.andExpect(jsonPath("$.suitableForVegetarians").value("false"));
+		
+		verify(mockMenuItemService, times(1)).createMenuItem(captor.getValue());
+	}
+	
+	@Test
+	public void testUpdateMenuItem_UpdatesMenuItemSuccessfully() {
+		// Arrange
+		int itemId = 1;
+		MenuItem updatedMenuItem = createMenuItem(itemId);
+		when(mockMenuItemService.editMenuItem(any(), eq(itemId))).thenReturn(updatedMenuItem);
+		
+		// Act
+		ResponseEntity<MenuItem> response = mockMenuItemController.updateMenuItem(itemId, updatedMenuItem);
+		
+		// Assert 
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals(updatedMenuItem, response.getBody());
+		verify(mockMenuItemService, times(1)).editMenuItem(updatedMenuItem, itemId);
+	}
+	
+	@Test
+	public void testDeleteMenuItemByItemId_DeletesMenuItemSuccessfully() {
+		// Arrange
+		int itemId = 1;
+		
+		// Act
+		ResponseEntity<Void> response = mockMenuItemController.deleteMenuItem(itemId);
+		
+		// Assert
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertNull(response.getBody());
+		verify(mockMenuItemService, times(1)).removeMenuItemById(itemId);
+	}
 }
